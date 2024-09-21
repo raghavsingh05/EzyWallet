7b8de9d6c82189aea92df033cf7e232c491aa3f0
raghavsingh05 <raghavsingh47336@gmail.com>
2024-09-21 11:53:32 +0530

    added prisma

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,10 @@
+import { PrismaClient } from "@repo/db/client";
+
+const client = new PrismaClient();
 
 export default function Home() {
   return (
-    <div className=" text-white bg-slate-900 h-screen text-5xl">
+    <div className=" text-white bg-slate-900 h-screen text-2xl">
       hi there
     </div>
   );
