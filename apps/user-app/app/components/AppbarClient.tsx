261bc0dbcc915d3580bf3c1f751410adb0f1f76c
raghavsingh05 <raghavsingh47336@gmail.com>
2024-10-18 11:38:33 +0530

    adds appbar client

--- apps/user-app/app/components/AppbarClient.tsx
+++ apps/user-app/app/components/AppbarClient.tsx
@@ -1,18 +1,18 @@
 "use client"
-
-import { signIn, signOut, useSession } from "next-auth/react"
+import { signIn, signOut, useSession } from "next-auth/react";
 import { Appbar } from "@repo/ui/appbar";
 import { useRouter } from "next/navigation";
 
-export function AppbarClient(){
-    const session = useSession();
-    const router = useRouter();
-    return (
-        <div>
-            <Appbar onSignin={signIn} onSignout={async ()=>{
-                await signOut()
-                router.push("/api/auth/signin")
-            }} user={session.data?.user}/>
-        </div>
-    )
+export function AppbarClient() {
+  const session = useSession();
+  const router = useRouter();
+
+  return (
+   <div>
+      <Appbar onSignin={signIn} onSignout={async () => {
+        await signOut()
+        router.push("/api/auth/signin")
+      }} user={session.data?.user} />
+   </div>
+  );
 }
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -6,7 +6,6 @@ export default function Page(): JSX.Element {
   const session = useSession();
   return (
    <div>
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
    </div>
   );
 }
