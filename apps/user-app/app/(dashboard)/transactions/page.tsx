7d3dd993d8e1deb9730cdebf7d6bfd0bbe048864
raghavsingh05 <raghavsingh47336@gmail.com>
2024-10-21 21:21:40 +0530

    Fix: minor bug

--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -1,6 +1,8 @@
 
-export default function() {
-    return <div>
-        Transactions
-    </div>
+export default function (){
+    return (
+        <div>
+            Transactions page
+        </div>
+    )
 }
--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -2,13 +2,13 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "../provider";
-import { AppbarClient } from "../components/AppbarClient";
+import { AppbarClient } from "./components/AppbarClient";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Wallet",
-  description: "Simple wallet app",
+  title: "OniWallet",
+  description: "An online wallet for all your transactions",
 };
 
 export default function RootLayout({
@@ -19,8 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className}>
-          <div className="min-w-screen min-h-screen bg-[#ebe6e6]">
+      <body className={inter.className}>
+          <div className="min-w-screen min-h-screen bg-[#f0f8f6]">
             <AppbarClient />
             {children}
           </div>
