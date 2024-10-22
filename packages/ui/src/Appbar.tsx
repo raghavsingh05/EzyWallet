5a80365f55be270a7133530847f61b59d8f6f7c1
raghavsingh05 <raghavsingh47336@gmail.com>
2024-10-22 17:01:01 +0530

    Add: appbar logo and favicon

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -1,4 +1,5 @@
 import { Button } from "./button";
+import Image from 'next/image';
 
 interface AppbarProps {
     user?: {
@@ -15,9 +16,8 @@ export const Appbar = ({
     onSignout
 }: AppbarProps) => {
     return <div className="flex justify-between border-b px-4 border-slate-300">
-        <div className="text-lg flex flex-col justify-center">
-            OniWallet
-        </div>
+
+        <Image src="/navbanner.png" alt="EZYWALLET" width={180} height={65}/>
         <div className="flex flex-col justify-center pt-2">
             <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
         </div>
