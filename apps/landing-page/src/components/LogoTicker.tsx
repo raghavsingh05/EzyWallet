197fba9341d8df4647092a6b0f1347298e8cd26e
raghavsingh05 <raghavsingh47336@gmail.com>
2024-12-24 16:05:00 +0530

    add: landing page logo ticker

--- apps/landing-page/src/components/LogoTicker.tsx
+++ apps/landing-page/src/components/LogoTicker.tsx
@@ -1,9 +1,11 @@
+'use client'
 import acmeLogo from "../assets/images/acme.png";
 import quantumLogo from "../assets/images/quantum.png";
 import echoLogo from "../assets/images/echo.png";
 import celestialLogo from "../assets/images/celestial.png";
 import pulseLogo from "../assets/images/pulse.png";
 import apexLogo from "../assets/images/apex.png";
+import Image from "next/image";
 
 const images = [
   { src: acmeLogo, alt: "Acme Logo" },
@@ -15,5 +17,18 @@ const images = [
 ];
 
 export const LogoTicker = () => {
-  return null;
+  return (
+    <div className="bg-black text-white py-[72px] sm:py-24">
+      <div className="container">
+        <h2 className="text-xl text-center text-white/70">Combined India's most trusted banks</h2>
+        <div className="overflow-hidden mt-9 before:content-[''] after:content-[''] before:absolute after:absolute before:h-full after:h-full sm:before:w-20 before:w-5 after:w-5 sm:after:w-20 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
+          <div className="flex gap-16 sm:gap-20 justify-center ">
+            {images.map(({ src, alt }) => (
+              <Image src={src} alt={alt} className="flex-none h-8 w-auto" />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
