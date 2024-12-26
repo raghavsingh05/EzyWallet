382ccd20c55ea85c319b28583eb57195c79127c5
raghavsingh05 <raghavsingh47336@gmail.com>
2024-12-26 23:27:57 +0530

    navcomponent update

--- apps/landing-page/src/components/Navbar.tsx
+++ apps/landing-page/src/components/Navbar.tsx
@@ -7,8 +7,7 @@ export const Navbar = () => {
   const router = useRouter();
   
   const handleGetStartedClick = () => {
-    // Redirect to the login page of your dashboard app (running on port 3001)
-    router.push('https://ezywallet.dashboard.rajraghav.com'); // Replace with actual login page URL if deployed
+    router.push('https://ezywallet.dashboard.rajraghav.com');
   };
   return <div className="bg-black"> 
   <div className="sm:px-8 px-4">
